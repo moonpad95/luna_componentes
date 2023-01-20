595273d9d28991f2505ef404ea1f81a9a04f6f24
Your Name <you@example.com>
2023-01-19 22:24:07 -0600

    componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
 
-function App() {
+const Button = styled.button`
+  display: inline-block;
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  display: block;
+`;
+function openFacebook(){
+  window.open("https://utd.edu.mx");
+}
+function openGit(){
+  window.open("https://github.com/moonpad95");
+}
+function openFace(){
+  window.open("https://www.facebook.com/luis.hernandezluna.2");
+}
+
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Como</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Button secondary as="a"  onClick={openFacebook}>Pagina oficial de la UTD</Button>
+      <Button secondary as="a"  onClick={openGit}>Mi GitHub</Button>
+      <Button secondary as="a"  onClick={openFace}>Mi facebook</Button>
+
+
+
+
     </div>
   );
 }
-
-export default App;
